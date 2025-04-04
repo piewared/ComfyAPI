1725c11f30498ee3ec49eecbe104dcea74735337
piewared <51264551+piewared@users.noreply.github.com>
2025-04-03 21:48:07 -0400

    checkpoint

--- runpod_entrypoint.sh
+++ runpod_entrypoint.sh
@@ -37,11 +37,26 @@ if [[ -v "PUBLIC_KEY" ]] && [[ ! -d "${HOME}/.ssh" ]]; then
   service ssh start
 fi
 
+# Create workspace directory if it doesn't exist, along with 'input', 'output', 'custom_nodes', and 'models' subdirectories
+mkdir -p "${COMFYUI_WORKSPACE_PATH}"
+mkdir -p "${COMFYUI_WORKSPACE_PATH}/input"
+mkdir -p "${COMFYUI_WORKSPACE_PATH}/output"
+mkdir -p "${COMFYUI_WORKSPACE_PATH}/custom_nodes"
+mkdir -p "${COMFYUI_WORKSPACE_PATH}/models"
+
+# if ${COMFYUI_WORKSPACE_PATH} is different from ${COMFYUI_INSTANCE_PATH}, copy custom_nodes from the instance to the workspace
+if [[ "${COMFYUI_WORKSPACE_PATH}" != "${COMFYUI_INSTANCE_PATH}" ]]; then
+  if [ -d "${COMFYUI_INSTANCE_PATH}/custom_nodes" ]; then
+    #ls "${COMFYUI_INSTANCE_PATH}/custom_nodes"
+    mv ${COMFYUI_INSTANCE_PATH}/custom_nodes/* ${COMFYUI_WORKSPACE_PATH}/custom_nodes/
+  fi
+fi
+
 
 # Run the CMD as the Container User (not root).
 #exec gosu ${USER} "$@"
 echo "Running as user: $(whoami)"
 echo "User ID: $(id -u)"
 echo "Group ID: $(id -g)"
-echo "Command: $@"
+echo "Command: $*"
 exec "$@"
